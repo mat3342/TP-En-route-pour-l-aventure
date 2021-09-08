0b1a27f053609b7da97277068539dfb97e192e7f
mattheo <mattheo.hennebert60@gmail.com>
2021-09-08 21:00:46 +0200

    fin de l'ex

--- Exercice 3.py
+++ Exercice 3.py
@@ -1,5 +1,8 @@
-print('hello world')
+temp_c= float(input('Choisir une température en degré'))
 
-a= 4
-b= 3
+def deg_far (temp_c):
+    temp_f = temp_c * 1.8 +32
 
+    print('La température en degré fareneight est de',temp_f,'°F')
+
+deg_far(temp_c)
