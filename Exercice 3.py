dc7b9c6a9e2321033c823cf012e0359e78ef7482
mattheo <mattheo.hennebert60@gmail.com>
2021-09-08 21:26:15 +0200

    fin de l'exv2

--- Exercice 3.py
+++ Exercice 3.py
@@ -1,8 +1,9 @@
-temp_c= float(input('Choisir une température en degré'))
+temp_c = float(input('Choisir une température en degré'))
 
-def deg_far (temp_c):
-    temp_f = temp_c * 1.8 +32
+def deg_far(x):
+    far = (x * 1.8) + 32
+    return far
 
-    print('La température en degré fareneight est de',temp_f,'°F')
+temp_f = deg_far(temp_c)
 
-deg_far(temp_c)
+print('La température en degré fareneight est de', float(temp_f), '°F')
